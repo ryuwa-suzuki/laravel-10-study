c58f56f2645eceecc62b83ff1a343fc0cbd2a53e
ryuwa <ryuusuzuki243@gmail.com>
2023-06-30 18:30:55 +0900

    add: shopify config

---
+++ config/shopify.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'client_key' => env('SHOPIFY_CLIENT_KEY'),
+    'secret_key' => env('SHOPIFY_SECRET_KEY'),
+    'shop_url' => env('SHOPIFY_SHOP_URL'),
+    'callback_url' => env('SHOPIFY_CALLBACK_URL')
+];

98e4ac8c27a97485a38c0d11932f9f6daada6211
ryuwa <ryuusuzuki243@gmail.com>
2023-09-24 16:09:17 +0900

    add: サンプルページ

---
+++ resources/js/Pages/Sample.tsx
@@ -0,0 +1,28 @@
+import { NavigationMenu } from "@shopify/app-bridge-react";
+// import {
+//   Card,
+//   EmptyState,
+//   Layout,
+//   Page,
+//   SkeletonBodyText,
+// } from "@shopify/polaris";
+const Sample = () => {
+  // function t(arg0: string): string {
+  //   throw new Error("Function not implemented.");
+  // }
+  // const { t } = useTranslation();
+
+  return (
+    <div>test</div>
+    // <NavigationMenu
+    //   navigationLinks={[
+    //     {
+    //       label: "NavigationMenu.pageName",
+    //       destination: "/pagename",
+    //     },
+    //   ]}
+    // />
+  );
+}
+
+export default Sample;
